8d892b4f2cff4a3f501d8c91787afcf8119530f4
frankvoc <fmvocatura@email.neit.edu>
2025-03-02 14:25:21 -0500

    Updated to MongoAtlas

--- E-Commerce-Auth/config.js
+++ E-Commerce-Auth/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    mongoURI: 'mongodb://localhost:27017/users',
+    mongoURI: 'mongodb+srv://FVoc:<FVocTest>@users.jwwwh.mongodb.net/?retryWrites=true&w=majority&appName=Users',
     jwtSecret: '8f8256878cd4bdd91646ea8a39443798e8312df5a13cffb66ad465607b7c4eb3'
   };
   
